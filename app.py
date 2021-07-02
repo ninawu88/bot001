6fdaf44c64b11ce62f40f34adb0ca67b8cb41510
bot001 <>
2021-07-02 04:04:46 +0000

    text_msg reply conditions have set in handle_msg
    TODO:
    1. add SQLalchemy

--- app.py
+++ app.py
@@ -12,17 +12,19 @@ from linebot.models import (
 
 app = Flask(__name__)
 
-line_bot_api = LineBotApi('YOUR_CHANNEL_ACCESS_TOKEN')
-handler = WebhookHandler('YOUR_CHANNEL_SECRET')
+line_bot_api = LineBotApi('Mey9WSMEsYoM9tPsG7cwWoF3VJQBgo7c0doI70rNl/XSMqqeVcU/KZxvtdE2JRyqSYOhXTQTSDl2ekWrEb2FbnRPAHEkwQLPdLgTmBZLKwUoIG6yw+ZS+7QUCq2aply5HI1Youl9VCt1SRqeOen44gdB04t89/1O/w1cDnyilFU=')
+handler = WebhookHandler('ad324cad12249807470d71979e3f12b8')
 
 
 @app.route("/callback", methods=['POST'])
 def callback():
+    #print(f"this is request.headers: {request.headers}")
+
     # get X-Line-Signature header value
     signature = request.headers['X-Line-Signature']
-
     # get request body as text
     body = request.get_data(as_text=True)
+    #print(f"this is request.body: {body}")
     app.logger.info("Request body: " + body)
 
     # handle webhook body
@@ -37,9 +39,26 @@ def callback():
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
+    #print(f"this is event: {event}")
+    msg_text = str(event.message.text).lower()
+    msg_reply = None
+
+    if msg_text in ['i am ready to order', 'add', '加入購物車']:
+        msg_reply = TextSendMessage(text='list products')
+    
+    elif msg_text in ['my cart', 'cart', '我的購物車']:
+        msg_reply = TextSendMessage(text='cart')
+
+    if msg_reply is None: 
+        msg_reply = [
+            TextSendMessage(text='''若要將商品加入購物車，請傳送訊息: 加入購物車\n若要顯示您的購物車，請傳送訊息: 我的購物車'''),
+            TextSendMessage(text='''If you would like to add the product to the cart, please type: "add" or "i am ready to order"'''),
+            TextSendMessage(text='''If you would like to see the products in your cart, please type: "my cart" or "cart"''')
+        ]
+
     line_bot_api.reply_message(
         event.reply_token,
-        TextSendMessage(text=event.message.text))
+        msg_reply)
 
 
 if __name__ == "__main__":
